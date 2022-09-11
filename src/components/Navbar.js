0d857d033e64e0d9bd65fb2cdc081bc7abd1fc6e
aones <aone.shrestha@selu.edu>
2022-09-11 04:19:54 -0500

    initial set up

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react'
+
+
+function Navbar() {
+  return (
+    <div>
+        <>
+        <nav className="Navbar">
+          <div className="navbar-container">
+            <Link to='/' className="navbar-logo">
+              CURRY
+
+
+            </Link>
+            
+            </div> 
+         </nav>
+        </>
+
+    </div>
+  )
+}
+
+export default Navbar
